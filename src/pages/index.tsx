c673dae983c6d6346ce134b80e7007f86028d43e
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-05-13 16:54:51 -0300

    chore: add home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+import {GetStaticProps} from 'next';
+import Head from 'next/head'
+import styles from './home.module.scss';
+import SubscribeButtton from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+import { useMemo } from 'react';
+
+interface HomeProps{
+  product: {
+    priceId: string;
+    amount: number
+  }
+}
+
+export default function Home({product}:HomeProps) {
+
+  const priceFormatted = useMemo(() =>{
+    return new Intl.NumberFormat('en-US', {
+      currency: 'USD',
+      style: 'currency'
+    }).format(product.amount)
+  }, [product.amount])
+
+  return (
+    <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <span>React</span> world,</h1>
+          <p>
+            Get access to all the publications <br /> 
+            <span>for {priceFormatted} per month</span>
+          </p>
+        <SubscribeButtton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="" />
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps:GetStaticProps = async () =>{
+
+  const price = await stripe.prices.retrieve('price_1IpZluAkzMQGnWsyE3bAAmsh')
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
+}
